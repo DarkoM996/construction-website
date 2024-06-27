76dfc1fc9e55ec84e3895afbe502ef8c71091fb2
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-27 15:50:37 +0200

    Initial

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const cormorant = Cormorant_Garamond({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font--bodoni--moda",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cormorant.className}>{children}</body>
     </html>
   );
 }
