2932e5ab77a425a595030de3b6f695bc813564ff
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-27 19:33:55 +0200

    menu opens and closes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   const links = [
     { url: "/", title: "Home" },
     { url: "/about", title: "About" },
@@ -15,22 +18,26 @@ const Navbar = () => {
       <Link href="/" className="text-2xl text-transform: uppercase">
         CamYork
       </Link>
-      {/* Right Side */}
-      <div>Item 2</div>
       {/* Responsive Menu */}
-      <div className="w-10 h-5 flex flex-col justify-between items-center">
-        {/* Menu Button */}
+      <div>Item 2</div>
+      {/* Menu Button */}
+      <div
+        className="w-10 h-5 flex flex-col justify-between items-center z-50 relative"
+        onClick={() => setOpen(!open)}
+      >
         <div className="w-10 h-1 bg-white rounded"></div>
         <div className="w-10 h-1 bg-white rounded"></div>
       </div>
       {/* Menu List */}
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center gap-8 text-6xl bg-primary_lighterColor-200 text-black ">
-        {links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
-        ))}
-      </div>
+      {open && (
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center gap-8 text-6xl bg-primary_lighterColor-200 text-black ">
+          {links.map((link) => (
+            <Link href={link.url} key={link.title}>
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
