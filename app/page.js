e05e885507f0b1dd947e8fff84efb9bc42bea3db
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-27 15:59:39 +0200

    Initial commit

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-5xl">Something</h1>
+      <h1 className="text-6xl">Something</h1>
     </main>
   );
 }
