9368de9b74cf2457760025e0162dcacd68f4feda
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-27 16:25:16 +0200

    Extending Tailwind with size and custom colors

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={cormorant.className}>
-        <div className="w-screen h-screen bg-black">{children}</div>
+        <div className="w-screen h-screen bg-primary_backgroundColor-900">
+          {children}
+        </div>
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        "3xl": "1920px",
+      },
+      colors: {
+        primary_backgroundColor: {
+          100: "#F1F1F1",
+          200: "#EDEDED",
+          300: "#E6E6E6",
+          400: "#E0E0E0",
+          500: "#D9D9D9",
+        },
+        primary_backgroundColor: {
+          600: "#262626",
+          700: "#1F1F1F",
+          800: "#191919",
+          850: "#121212",
+          900: "#0D0C0C",
+        },
       },
     },
   },
