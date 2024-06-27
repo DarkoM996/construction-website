c735675deba62b6001c525a35610b8799d848546
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-27 19:46:37 +0200

    latest

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -18,17 +18,23 @@ const Navbar = () => {
       <Link href="/" className="text-2xl text-transform: uppercase">
         CamYork
       </Link>
-      {/* Responsive Menu */}
-      <div>Item 2</div>
+      {/* Responsive Menu on Desktop */}
+      <div className="hidden md:flex justify-center items-center gap-4 text-xl">
+        {links.map((link) => (
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
       {/* Menu Button */}
       <div
-        className="w-10 h-5 flex flex-col justify-between items-center z-50 relative"
+        className="md:hidden w-10 h-5 flex flex-col justify-between items-center z-50 relative"
         onClick={() => setOpen((prev) => !prev)}
       >
         <div className="w-10 h-1 bg-white rounded"></div>
         <div className="w-10 h-1 bg-white rounded"></div>
       </div>
-      {/* Menu List */}
+      {/* Mobile Menu List */}
       {open && (
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center gap-8 text-6xl bg-primary_lighterColor-200 text-black ">
           {links.map((link) => (
