b980514991614523ed85c720c2dd6613762b7954
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-06-30 17:23:10 +0200

    Initial attempt.. kinda off target

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,6 +7,27 @@ import { motion } from "framer-motion";
 function Navbar() {
   const [open, setOpen] = useState(false);
 
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(0,0,0)",
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(0,0,0)",
+    },
+  };
+
   const links = [
     { url: "/", title: "Home" },
     { url: "/about", title: "About" },
@@ -35,8 +56,17 @@ function Navbar() {
             className="md:hidden w-10 h-5 flex flex-col justify-between items-center z-50 relative"
             onClick={() => setOpen((prev) => !prev)}
           >
-            <motion.div className="w-10 h-1 bg-white rounded"></motion.div>
-            <motion.div className="w-10 h-1 bg-white rounded"></motion.div>
+            {/* the animate part works with conditionals. if it's open, use the opened variants.. else use the other variable. */}
+            <motion.div
+              variants={topVariants}
+              animate={open ? "opened" : "closed"}
+              className="w-10 h-1 bg-white rounded"
+            ></motion.div>
+            <motion.div
+              variants={bottomVariants}
+              animate={open ? "opened" : "closed"}
+              className="w-10 h-1 bg-white rounded"
+            ></motion.div>
           </div>
           {/* Mobile Menu List */}
           {/* Z index added so the menu can stay above the hero image */}
