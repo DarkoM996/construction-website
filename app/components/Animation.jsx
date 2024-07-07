04c712dd4bf871914cc57145166151d17e0ab1a3
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-07 16:37:05 +0200

    Working animation

---
+++ app/components/Animation.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import picture1 from "../../public/images/picture1.jpg";
+import picture2 from "../../public/images/picture2.jpg";
+import picture3 from "../../public/images/picture3.jpg";
+import picture4 from "../../public/images/picture4.jpg";
+import picture5 from "../../public/images/picture5.jpg";
+import picture6 from "../../public/images/picture6.jpg";
+import picture7 from "../../public/images/picture7.jpg";
+
+const logos = [
+  { src: picture1, alt: "Facebook" },
+  { src: picture2, alt: "Disney" },
+  { src: picture3, alt: "Airbnb" },
+  { src: picture4, alt: "Apple" },
+  { src: picture5, alt: "Spark" },
+  { src: picture6, alt: "Samsung" },
+  { src: picture7, alt: "Quora" },
+];
+
+const Animation = () => {
+  return (
+    <div className="max-w-screen-3xl mx-auto h-screen relative px-6 py-4">
+      Animation
+      <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+          {logos.map((logo, index) => (
+            <li key={index}>
+              <Image src={logo.src} alt={logo.alt} />
+            </li>
+          ))}
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          {logos.map((logo, index) => (
+            <li key={index}>
+              <Image src={logo.src} alt={logo.alt} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import HoverImageLinks from "./components/HoverImageLinks";
+import Animation from "./components/Animation";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HoverImageLinks />
+      <Animation />
     </>
   );
 }
