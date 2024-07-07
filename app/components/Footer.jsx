c80319808840129b7bc3008ca8d318fa4898736f
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-07 23:41:29 +0200

    Adding comments

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,11 +4,13 @@ import { FiFacebook, FiInstagram, FiTwitter, FiYoutube } from "react-icons/fi";
 const Footer = () => {
   return (
     <footer className="max-w-screen-3xl mx-auto w-full h-[80%] relative">
+      {/* CTA centered in the middle */}
       <div className="flex flex-col justify-center items-center w-full h-full ">
         <h2 className="text-5xl md:text-6xl lg:text-8xl items-center">
           Contact Us
         </h2>
       </div>
+      {/* Bottom part section */}
       <div className="flex flex-col md:flex-row justify-between items-center gap-8 px-6 py-4 w-full h-fit bg-black">
         <p className="text-transform: uppercase">Privacy Policy</p>
         <p>@2024 CamYork</p>
