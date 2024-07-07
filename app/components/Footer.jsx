8a401e992eb76a1a93b811dbdf5b35c745dbb799
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-07 23:36:05 +0200

    working solution

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { FiFacebook, FiInstagram, FiTwitter, FiYoutube } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 bg-primary_backgroundColor-800">
-      <h2 className="text-5xl md:text-6xl lg:text-8xl">Contact Us</h2>
-      <div></div>
+    <footer className="max-w-screen-3xl mx-auto w-full h-[80%] relative bg-primary_backgroundColor-800">
+      <div className="flex flex-col justify-center items-center w-full h-full ">
+        <h2 className="text-5xl md:text-6xl lg:text-8xl items-center py-64">
+          Contact Us
+        </h2>
+        <div className="flex flex-col md:flex-row justify-between items-center gap-8 px-6 py-4 bg-black w-full h-fit">
+          <p className="text-transform: uppercase">Privacy Policy</p>
+          <p>@2024 CamYork</p>
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+            <FiInstagram className="text-2xl" />
+            <FiFacebook className="text-2xl" />
+            <FiYoutube className="text-2xl" />
+            <FiTwitter className="text-2xl" />
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
