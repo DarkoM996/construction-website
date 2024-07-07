0c365db2bf152d5eb7486f638ad047bf5e05f656
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-07 23:38:57 +0200

    color changes to original

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,13 +3,13 @@ import { FiFacebook, FiInstagram, FiTwitter, FiYoutube } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className="max-w-screen-3xl mx-auto w-full h-[80%] relative bg-primary_backgroundColor-800">
+    <footer className="max-w-screen-3xl mx-auto w-full h-[80%] relative">
       <div className="flex flex-col justify-center items-center w-full h-full ">
         <h2 className="text-5xl md:text-6xl lg:text-8xl items-center">
           Contact Us
         </h2>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-center gap-8 px-6 py-4 bg-black w-full h-fit">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-8 px-6 py-4 w-full h-fit">
         <p className="text-transform: uppercase">Privacy Policy</p>
         <p>@2024 CamYork</p>
         <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
