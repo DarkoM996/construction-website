f8bf80b71af228111fd70216a419f142c3a08371
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-07 17:01:05 +0200

    Going to the original design after all

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import HoverImageLinks from "./components/HoverImageLinks";
-import Animation from "./components/Animation";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HoverImageLinks />
-      <Animation />
     </>
   );
 }
