450ec2a2b58030e484bb9b22b20bfa73a169f5a8
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-16 15:30:27 +0200

    Initial new section added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutHero from "../components/AboutHero";
+import AboutValue from "../components/AboutValue";
 
 const page = () => {
   return (
     <>
       <AboutHero />
+      <AboutValue />
     </>
   );
 };
---
+++ app/components/AboutValue.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutValue = () => {
+  return (
+    <div className="max-w-screen-3xl mx-auto w-full h-full relative px-6 py-4">
+      AboutValue
+    </div>
+  );
+};
+
+export default AboutValue;
