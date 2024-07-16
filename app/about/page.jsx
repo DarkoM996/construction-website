2bb21d18a241d6e99d6e666c3b671d57ddb99737
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-16 22:33:06 +0200

    added footer

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutHero from "../components/AboutHero";
 import AboutValue from "../components/AboutValue";
+import Footer from "../components/Footer";
 
 const page = () => {
   return (
     <>
       <AboutHero />
       <AboutValue />
+      <Footer />
     </>
   );
 };
