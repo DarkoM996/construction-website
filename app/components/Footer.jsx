d4839cb68febd1fe8e3894226506ac74ebada371
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-16 13:57:47 +0200

    Adjustments in the footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="max-w-screen-3xl mx-auto w-full h-3/4 relative">
+    <footer className="max-w-screen-3xl mx-auto w-full h-3/4 relative px-6 py-4">
       {/* CTA centered in the middle */}
       <div className="flex flex-col justify-center items-center w-full h-full ">
-        <h2 className="text-5xl md:text-6xl lg:text-8xl items-center">
+        <Link
+          href="/contact"
+          className="text-5xl md:text-6xl lg:text-8xl items-center text-primary_lighterColor-200 hover:text-primary_customGrey-300"
+        >
           Contact Us
-        </h2>
+        </Link>
       </div>
       {/* Bottom part section */}
       <div className="flex flex-col md:flex-row justify-between items-center gap-8 px-6 py-8 w-full h-fit">
