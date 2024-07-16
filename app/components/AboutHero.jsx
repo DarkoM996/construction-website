ccf73da82b865efee7ffe8c1349f34a7dd743742
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-16 14:32:42 +0200

    Initial prototyping done

--- app/components/AboutHero.jsx
+++ app/components/AboutHero.jsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import backgroundimg from "../../public/images/picture6.jpg";
 
 const AboutHero = () => {
   return (
     <div className="max-w-screen-3xl mx-auto w-full min-h-screen relative px-6 py-4">
-      AboutHero
+      {/* Container Setup with flex */}
+      <h1>Studio</h1>
+      {/* Image Container */}
+      <div className="relative h-[500px]">
+        <Image
+          priority={true}
+          src={backgroundimg}
+          alt="/"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      {/* Paragraph talking about who the company is */}
+      <h2>
+        We are an experienced team of construction professionals, and designers
+        operating within Canada, with a combined 24 years of industry
+        experience.
+      </h2>
     </div>
   );
 };
