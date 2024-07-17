2693068eac15b3ce6364d54d1ce9bb43f40b5b4c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-17 22:33:41 +0200

    latest

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-[872px] px-6 py-4 md:py-12 lg:py-28 bg-primary_lighterColor-500">
+    <div className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-80px)] px-6 py-4 md:py-12 lg:py-28 bg-primary_lighterColor-500">
       <h2 className="text-4xl md:text-5xl lg:text-6xl py-12">
         Selected Projects
       </h2>
