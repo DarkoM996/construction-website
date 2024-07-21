04afaa8f5422dcabf3edd7025a33ec32d63c6b1e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 12:26:48 +0200

    latest

--- app/components/ContactSections.jsx
+++ app/components/ContactSections.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const ContactSections = () => {
   return (
-    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:py-8 lg:px-28 ">
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-8 py-6 md:py-8 lg:px-28 ">
       {/* Entire div */}
-      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-start gap-6">
         <h2 className="text-4xl md:text-5xl lg:text-6xl">Vancouver</h2>
         {/* Paragraph on the right */}
-        <div className="flex flex-col justify-center items-center gap-6 w-full md:w-2/5 lg:w-1/3">
+        <div className="flex flex-col justify-center items-center gap-6 w-full md:w-1/2 lg:w-1/3">
           {/* First item */}
           <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_customGrey-900 py-4 w-full">
             <div>Phone</div>
