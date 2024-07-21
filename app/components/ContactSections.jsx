b3e7df2128d98ef0c0de431bd3ea0a3f134e062c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 12:13:13 +0200

    finally got it to work

--- app/components/ContactSections.jsx
+++ app/components/ContactSections.jsx
@@ -7,7 +7,7 @@ const ContactSections = () => {
       <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
         <h2 className="text-4xl md:text-5xl lg:text-6xl">Vancouver</h2>
         {/* Paragraph on the right */}
-        <div className="flex flex-col justify-center items-center gap-6 w-full md:w-1/3">
+        <div className="flex flex-col justify-center items-center gap-6 w-full md:w-2/5 lg:w-1/3">
           {/* First item */}
           <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_customGrey-900 py-4 w-full">
             <div>Phone</div>
