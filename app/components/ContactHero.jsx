8dfebf456eb769d3fafac2ce3f7dc52c92ba3627
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 10:33:53 +0200

    Working section for now

--- app/components/ContactHero.jsx
+++ app/components/ContactHero.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const ContactHero = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full min-h-screen relative px-6 py-4 lg:px-28">
-      <div className="flex flex-col justify-center items-center w-full h-full gap-12 md:gap-16  pt-32">
-        <h1 className="text-5xl md:text-6xl lg:text-8xl">Contact</h1>
+    <div className="max-w-screen-3xl mx-auto w-full h-2/3 relative px-6 py-4 lg:px-28">
+      <div className="flex flex-col justify-center items-center w-full h-full gap-12 md:gap-16 ">
+        <h1 className="text-5xl md:text-6xl lg:text-8xl text-transform: uppercase">
+          Contact
+        </h1>
         <div className="flex flex-col md:flex-row justify-center items-center text-xl gap-8">
           <h2>Facebook</h2>
           <h2>Instagram</h2>
