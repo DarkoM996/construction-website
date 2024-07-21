0cba6dd25a27d4bad8c56c25f39a7495312123fa
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 11:53:28 +0200

    items alignment

--- app/components/ContactSections.jsx
+++ app/components/ContactSections.jsx
@@ -3,21 +3,23 @@ import React from "react";
 const ContactSections = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:py-8 lg:px-28 ">
-      <div className="flex flex-col sm:flex-row justify-between items-center md:items-start gap-6">
+      {/* Entire div */}
+      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
         <h2 className="text-4xl md:text-5xl lg:text-6xl">Vancouver</h2>
-        <div className="flex flex-col justify-center items-center gap-6">
+        {/* Paragraph on the right */}
+        <div className="flex flex-col justify-center items-center gap-6 w-1/3">
           {/* First item */}
-          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4">
+          <div className="flex flex-col md:flex-row justify-start items-center gap-6 text-xl border-b border-primary_customGrey-900 py-4 w-full">
             <div>Phone</div>
             <div>065 887 999</div>
           </div>
           {/* Second item */}
-          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4">
+          <div className="flex flex-col md:flex-row justify-start gap-6 text-xl border-b border-primary_customGrey-900 py-4 w-full">
             <div>Email</div>
             <div>contact@camyorkconstruction.com</div>
           </div>
           {/* Third item */}
-          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4">
+          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4 w-full">
             <div>Address</div>
             <div>Vancouver Beach 125 Str. </div>
           </div>
