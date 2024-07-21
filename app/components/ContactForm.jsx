693bc585bda1f3eef542ef0154a0f7a25006929e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 15:30:52 +0200

    getting closer

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -4,10 +4,10 @@ const ContactForm = () => {
   return (
     <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 lg:px-28">
       {/* Entire div */}
-      <div>
+      <div className="flex flex-col md:flex-row justify-between items-start gap-6">
         <h2>Order a Call</h2>
         {/* Contact Form on the right */}
-        <div className="max-w-screen-md mx-auto w-full h-[600px] relative">
+        <div className="flex flex-col gap-6 w-full md:w-1/2 lg:w-1/3">
           <label htmlFor="name">Name</label>
           <label htmlFor="email">Name</label>
           <label htmlFor="name">Name</label>
