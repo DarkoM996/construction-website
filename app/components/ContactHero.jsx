c317417507ff89e7354a35af7817477166ecd664
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 10:22:29 +0200

    latest

--- app/components/ContactHero.jsx
+++ app/components/ContactHero.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const ContactHero = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 lg:px-28">
-      ContactHero
+    <div className="max-w-screen-3xl mx-auto w-full h-[70%] relative px-6 py-4 lg:px-28">
+      <div className="flex flex-col justify-center items-center w-full h-full gap-12 md:gap-16  pt-32">
+        <h1 className="text-5xl md:text-6xl lg:text-8xl">Contact</h1>
+        <div className="flex flex-col md:flex-row justify-center items-center text-xl gap-8">
+          <h2>Facebook</h2>
+          <h2>Instagram</h2>
+          <h2>YouTube</h2>
+          <h2>Twitter</h2>
+          <h2>Pinterest</h2>
+        </div>
+      </div>
     </div>
   );
 };
