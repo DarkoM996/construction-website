f509c9aeb2db58c5ded1a9e2a8d3120b851837f0
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 10:03:00 +0200

    work begins

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ContactHero from "../components/ContactHero";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <ContactHero />
+    </>
+  );
 };
 
 export default page;
