512450270a7470f9a60bbdcbbac6c8fd21f58120
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 15:54:12 +0200

    colors changed

--- app/components/ContactSection.jsx
+++ app/components/ContactSection.jsx
@@ -9,12 +9,12 @@ const ContactSection = () => {
         {/* Paragraph on the right */}
         <div className="flex flex-col justify-center items-center gap-6 w-full md:w-1/2 lg:w-1/3">
           {/* First item */}
-          <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_backgroundColor-600 py-4 w-full">
+          <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_customGrey-800 py-4 w-full">
             <div>Phone</div>
             <div>065 887 999</div>
           </div>
           {/* Second item */}
-          <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_backgroundColor-600 py-4 w-full">
+          <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_customGrey-800 py-4 w-full">
             <div>Email</div>
             <div className="flex flex-row md:flex-col lg:flex-row justify-start items-start w-full">
               contact@
@@ -24,7 +24,7 @@ const ContactSection = () => {
             </div>
           </div>
           {/* Third item */}
-          <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_backgroundColor-600 py-4 w-full">
+          <div className="flex flex-col md:flex-row justify-start items-start gap-6 text-xl border-b border-primary_customGrey-800 py-4 w-full">
             <div>Address</div>
             <div>Vancouver Beach 125 Str. </div>
           </div>
