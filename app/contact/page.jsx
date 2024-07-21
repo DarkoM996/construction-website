d4de8181a99013ead265646c014a8b4e8ec49644
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 15:18:06 +0200

    new form added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
 import ContactHero from "../components/ContactHero";
 import ContactSection from "../components/ContactSection";
-import ContactSections from "../components/ContactSections";
 
 const page = () => {
   return (
     <>
       <ContactHero />
       <ContactSection />
+      <ContactForm />
     </>
   );
 };
