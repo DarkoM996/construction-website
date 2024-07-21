966c5227abc2159216c79a626c3b65d835d69c59
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 11:00:04 +0200

    Moving closer to the design

--- app/components/ContactSections.jsx
+++ app/components/ContactSections.jsx
@@ -3,9 +3,20 @@ import React from "react";
 const ContactSections = () => {
   return (
     <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:py-8 lg:px-28 ">
-      <div>
+      <div className="flex justify-between items-center gap-6">
         <h2>Vancouver</h2>
         <div>
+          {/* One item */}
+          <div className="flex gap-6">
+            <div>Phone</div>
+            <div>065 887 999</div>
+          </div>
+          {/* Second item */}
+          <div className="flex gap-6">
+            <div>Phone</div>
+            <div>065 887 999</div>
+          </div>
+          {/* Third item */}
           <div className="flex gap-6">
             <div>Phone</div>
             <div>065 887 999</div>
