7457e23a91665e57d27c886b5c76d026259bb5d8
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 11:28:02 +0200

    latest

--- app/components/ContactSections.jsx
+++ app/components/ContactSections.jsx
@@ -4,9 +4,9 @@ const ContactSections = () => {
   return (
     <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:py-8 lg:px-28 ">
       <div className="flex justify-between items-center gap-6">
-        <h2>Vancouver</h2>
+        <h2 className="text-4xl md:text-5xl lg:text-6xl">Vancouver</h2>
         <div>
-          {/* One item */}
+          {/* First item */}
           <div className="flex gap-6">
             <div>Phone</div>
             <div>065 887 999</div>
