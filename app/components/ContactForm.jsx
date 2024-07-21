ba0c43977d09a6d725b5a4e853d136bff0711532
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 15:45:38 +0200

    latest

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -2,24 +2,30 @@ import React from "react";
 
 const ContactForm = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 lg:px-28">
+    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-8 py-6 md:px-12 md:py-8 lg:px-28">
       {/* Entire div */}
       <div className="flex flex-col md:flex-row justify-between items-start gap-6">
         <h2>Order a Call</h2>
         {/* Contact Form on the right */}
-        <div className="flex flex-col gap-6 w-full md:w-1/2 lg:w-1/3">
+        <div className="flex flex-col justify-start items-start gap-6 w-full md:w-1/2 lg:w-1/3">
           <input
             type="text"
             placeholder="Name"
             name="name"
-            className="my-4 p-2 bg-primary_backgroundColor-900 border-b border-primary_customGrey-800"
+            className="my-2 p-2 bg-primary_backgroundColor-900 border-b border-primary_customGrey-800 w-full"
           />
           <input
             type="text"
             placeholder="Name"
             name="name"
-            className="my-4 p-2 bg-primary_backgroundColor-900 border-b border-primary_customGrey-800"
+            className="my-2 p-2 bg-primary_backgroundColor-900 border-b border-primary_customGrey-800 w-full"
           />
+          <textarea
+            className="bg-primary_backgroundColor-900 p-2 my-2 border-b border-primary_customGrey-800  w-full"
+            name="message"
+            rows="10"
+            placeholder="Your message here"
+          ></textarea>
 
           <button className="p-4 bg-black border border-primary_customGrey-950">
             Order a Call
