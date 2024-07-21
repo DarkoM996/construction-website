00cb542736f72042c96af3e4e05e501e651827aa
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 11:38:53 +0200

    Jury is out on the positioning for mobile

--- app/components/ContactSections.jsx
+++ app/components/ContactSections.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const ContactSections = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:py-8 lg:px-28 ">
-      <div className="flex justify-between items-center gap-6">
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 md:py-8 lg:px-28 ">
+      <div className="flex flex-col sm:flex-row justify-between items-center md:items-start gap-6">
         <h2 className="text-4xl md:text-5xl lg:text-6xl">Vancouver</h2>
-        <div>
+        <div className="flex flex-col justify-center items-center gap-6">
           {/* First item */}
-          <div className="flex gap-6">
+          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4">
             <div>Phone</div>
             <div>065 887 999</div>
           </div>
           {/* Second item */}
-          <div className="flex gap-6">
+          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4">
             <div>Phone</div>
             <div>065 887 999</div>
           </div>
           {/* Third item */}
-          <div className="flex gap-6">
+          <div className="flex gap-6 text-xl border-b border-primary_customGrey-900 py-4">
             <div>Phone</div>
             <div>065 887 999</div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
