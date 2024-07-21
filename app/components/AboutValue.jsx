8a8d991e52e7812a8b498571bd7657ccf1400960
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 16:21:27 +0200

    spacing added

--- app/components/AboutValue.jsx
+++ app/components/AboutValue.jsx
@@ -31,7 +31,7 @@ const AboutValue = () => {
         </div>
       </div>
       {/* Grid #2 */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-16 py-8 md:py-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-16 py-8 md:py-12 lg:py-24">
         {/* Paragraph */}
         <h2 className="text-4xl md:text-5xl lg:text-6xl">What We Do</h2>
         {/* Image */}
