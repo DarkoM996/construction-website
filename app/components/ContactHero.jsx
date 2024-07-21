c5d86a251b86853d13533738d669cd91768f1c8e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 10:03:55 +0200

    latest

--- app/components/ContactHero.jsx
+++ app/components/ContactHero.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ContactHero = () => {
   return (
-    <div>ContactHero</div>
-  )
-}
+    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 lg:px-28">
+      ContactHero
+    </div>
+  );
+};
 
-export default ContactHero
+export default ContactHero;
