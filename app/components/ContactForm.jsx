e6c0c1a043bf0174cee84cd9f70b7137c6f9e73d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-21 15:18:55 +0200

    latest

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <div className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-4 lg:px-28">
+      ContactForm
+    </div>
+  );
 };
 
 export default ContactForm;
