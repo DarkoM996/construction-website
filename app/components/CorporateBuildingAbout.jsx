924483b63600ba6bcc2ba0c10160b1ed868dbf38
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-07-23 11:21:58 +0200

    corrections

--- app/components/CorporateBuildingAbout.jsx
+++ app/components/CorporateBuildingAbout.jsx
@@ -5,10 +5,10 @@ const CorporateBuildingAbout = () => {
     <section className="max-w-screen-3xl mx-auto w-full h-[70%] relative px-6 py-4 md:py-10 lg:pb-24 lg:px-28">
       <div className="flex flex-col lg:flex-row justify-between items-start py-8 gap-6 lg:gap-32 lg:py-16">
         <h2 className="text-4xl md:text-5xl lg:text-6xl w-full lg:w-2/3">
-          We recently led the development of Building X, a premium building that
-          we made for a client in Vancouver, Canada. This was made by the latest
-          standards, with emphaphis on ultra high quality and well within the
-          appropriate time.
+          We recently led the development of Vancouver Residence, a premium
+          building that we made for a client in Vancouver, Canada. This was made
+          by the latest standards, with emphaphis on ultra high quality and well
+          within the appropriate time.
         </h2>
         <div className="w-full sm:w-1/2 lg:w-1/2">
           {/* First item */}
